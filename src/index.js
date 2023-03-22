309dbb156fa4842d3f8dd7f5bca5129a1532d9d7
Bryant Lin <bryantmlin@gmail.com>
2023-03-22 14:08:01 -0600

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createList(e.target.new_todo.value)
+    //console.log(e.target.new_todo.value)
+    form.reset()
+  })
 });
+
+function createList(item) {
+  //console.log(item)
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = '   x'
+  p.textContent = item
+  p.appendChild(btn)
+  //console.log(p)
+  document.querySelector("#list").appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
